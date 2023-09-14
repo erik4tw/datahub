adb493b4e44acca1ff2577492f53ba09d3ba3e1f
LeoKle <leon.kleinschmidt@outlook.de>
2023-09-14 15:10:24 +0200

    add workflows

---
+++ .github/workflows/main.py
@@ -0,0 +1,17 @@
+from formatJsons import format_and_sort_json_files
+from consolidateData import consolidateJsons
+from createTsBotJson import createTeamspeakStationBotJson
+
+combinedFile = "data.json"
+folders_to_sort = ['edgg', 'edmm', 'eduu', 'edww', 'edyy', 'bookingsystem']
+folders_to_consolidate = ['edgg', 'edmm', 'eduu', 'edww', 'edyy']
+
+# format all source jsons
+format_and_sort_json_files(folders_to_sort)
+
+# consolidate all source files into one combined file
+consolidateJsons(folders_to_consolidate, combinedFile)
+
+# create legacy jsons
+createTeamspeakStationBotJson(combinedFile, "legacy/atc_station_mappings.json")
+
